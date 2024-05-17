a668e90bd36cd52ef9257f444a78f63e51bb84c0
SujithaNagasuri <124752663+SujithaNagasuri@users.noreply.github.com>
2024-05-17 18:43:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.accordion-item,.about-intro,.right,.card', { origin: 'right' });
+ScrollReveal().reveal('.about-img,.quote, .left', { origin: 'left' });
+ScrollReveal().reveal('.heading', { origin: 'top' });
+
+
+
